755fdd327f6361102b6a0b60f0fc2e7214a58dca
Xing Yang <xor@jecity.net>
2023-02-11 11:46:12 +0000

    better audio functionalites

--- roles/laptop/audio.nix
+++ roles/laptop/audio.nix
@@ -8,25 +8,17 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    media-session.enable = true;
-    wireplumber.enable = false;
+    wireplumber.enable = true;
+  };
 
-    media-session.config.bluez-monitor.rules = [
-      {
-        matches = [{ "device.name" = "~bluez_card.*"; }];
-        actions = {
-          "update-props" = {
-            "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
-            "bluez5.msbc-support" = true;
-          };
-        };
+  environment.etc = {
+    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
       }
-      {
-        matches = [
-          { "node.name" = "~bluez_input.*"; }
-          { "node.name" = "~bluez_output.*"; }
-        ];
-      }
-    ];
+    '';
   };
 }
