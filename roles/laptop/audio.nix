29c28166dd3a3a988b4a7f3527a01c0a7e5733ae
Xing Yang <xor@jecity.net>
2023-02-22 13:51:40 +0000

    Fix Headfree profile not available problem with Sony WH1000XWM5.

--- roles/laptop/audio.nix
+++ roles/laptop/audio.nix
@@ -17,7 +17,7 @@
         ["bluez5.enable-sbc-xq"] = true,
         ["bluez5.enable-msbc"] = true,
         ["bluez5.enable-hw-volume"] = true,
-        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+        ["bluez5.headset-roles"] = "[ hsp_hs hfp_hf hfp_ag ]"
       }
     '';
   };
