518244124e744ae2af6ca7724d479e80652ef059
Xing Yang <xor@jecity.net>
2023-03-28 20:39:22 +0100

    add rclone conf

--- roles/home/secrets.nix
+++ roles/home/secrets.nix
@@ -1,9 +1,17 @@
 { config, ... }:
 {
   sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
-  sops.secrets.git_user_inc = {
-    format = "yaml";
-    sopsFile = ./secrets/git.yaml;
-    path = "${config.home.homeDirectory}/.config/git/user.inc";
+  sops.secrets = {
+    git_user_inc = {
+      format = "yaml";
+      sopsFile = ./secrets/git.yaml;
+      path = "${config.home.homeDirectory}/.config/git/user.inc";
+    };
+
+    "rclone.conf" = {
+      format = "binary";
+      sopsFile = ./secrets/rclone.conf;
+      path = "${config.home.homeDirectory}/.config/rclone/rclone.conf";
+    };
   };
 }
