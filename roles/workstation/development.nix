55c9ba3946b674bc9c7dc96514993cb4c201f9f3
Xing Yang <xor@jecity.net>
2023-07-14 09:17:12 +0100

    use rootless docker

--- roles/workstation/development.nix
+++ roles/workstation/development.nix
@@ -1,7 +1,15 @@
 { config, pkgs, ... }:
 {
-  virtualisation.docker.enable = true;
-  users.users.${config.primaryUser}.extraGroups = [ "docker" ];
+  virtualisation.docker = {
+    enable = true;
+    autoPrune.enable = true;
+
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+
   boot.binfmt.emulatedSystems = [
     "aarch64-linux"
   ];
