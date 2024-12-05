c3e9d7c60ad646668536b1c44c2c83798efc979d
Xing Yang <xor@jecity.net>
2024-12-05 19:51:09 +0000

    enable basic nvidia power management

--- boxes/desktop/hardware.nix
+++ boxes/desktop/hardware.nix
@@ -58,6 +58,7 @@
   };
 
   hardware.nvidia = {
+    powerManagement.enable = true;
     powerManagement.finegrained = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.production;
