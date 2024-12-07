57cdb341a8f7b3f170d81092d845e904be79f13a
Xing Yang <xor@jecity.net>
2024-12-07 14:30:01 +0000

    improve amd cpu support

--- boxes/desktop/default.nix
+++ boxes/desktop/default.nix
@@ -10,6 +10,10 @@
 
         config.flake.nixosModules.workstation
         inputs.sops-nix.nixosModules.sops
+
+        inputs.nixos-hardware.nixosModules.common-cpu-amd
+        inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+        inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
       ];
     };
 }
