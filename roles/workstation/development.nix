6ffad2a2c391cdd53f0aa04f97fed4819519b260
Xing Yang <xor@jecity.net>
2024-12-14 19:39:59 +0000

    getting cuda work in containers

--- roles/workstation/development.nix
+++ roles/workstation/development.nix
@@ -1,14 +1,27 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let
+  docker = pkgs.docker.override rec {
+    version = "27.4.0";
+    mobyRev = "v${version}";
+    mobyHash = "sha256-lhl4YkzOTTvddS4fleEif7z996yJ2ON6S1SnPU+owzM=";
+    cliRev = "v${version}";
+    cliHash = "sha256-q6xKERB5K7idExTrwFfX2ORs2G/55s2pybyhPcV5wuo=";
+  };
+in
 {
   virtualisation.docker = {
     enable = true;
+    package = docker;
     autoPrune.enable = true;
 
     rootless = {
       enable = true;
       setSocketVariable = true;
+      package = docker;
     };
   };
+
+  hardware.nvidia-container-toolkit.enable = true;
 
   boot.binfmt.emulatedSystems = [
     "aarch64-linux"
