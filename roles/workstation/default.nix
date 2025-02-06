4408ab32826191c758c5e61527413889d1341ce1
Xing Yang <xor@jecity.net>
2025-02-06 10:53:31 +0000

    use docker from unstable

--- roles/workstation/default.nix
+++ roles/workstation/default.nix
@@ -1,9 +1,15 @@
-{ ... }:
+{ inputs, ... }:
 {
-  flake.nixosModules.workstation = { ... }: {
+  flake.nixosModules.workstation = { config, ... }: {
     imports = [
       ./main.nix
       ../common.nix
+    ];
+
+    nixpkgs.overlays = [
+      (final: prev: {
+        docker = inputs.nixpkgs-unstable.legacyPackages.${config.nixpkgs.hostPlatform.system}.docker;
+      })
     ];
   };
 }
--- roles/workstation/development.nix
+++ roles/workstation/development.nix
@@ -1,23 +1,12 @@
 { config, pkgs, lib, ... }:
-let
-  docker = pkgs.docker.override rec {
-    version = "27.4.0";
-    mobyRev = "v${version}";
-    mobyHash = "sha256-lhl4YkzOTTvddS4fleEif7z996yJ2ON6S1SnPU+owzM=";
-    cliRev = "v${version}";
-    cliHash = "sha256-q6xKERB5K7idExTrwFfX2ORs2G/55s2pybyhPcV5wuo=";
-  };
-in
 {
   virtualisation.docker = {
     enable = true;
-    package = docker;
     autoPrune.enable = true;
 
     rootless = {
       enable = true;
       setSocketVariable = true;
-      package = docker;
     };
   };
 
