af6d1b6516ade79efa095435b9a270672fbd3510
Xing Yang <xor@jecity.net>
2025-05-26 12:42:12 +0100

    switch from rpi to x86 mini pc (#11)

--- images/default.nix
+++ images/default.nix
@@ -1,8 +1,32 @@
-{ ... }:
+{ withSystem, inputs, config, ... }:
+let
+  image = { system, format }:
+    withSystem system
+      (
+        { pkgs, ... }:
+        inputs.nixos-generators.nixosGenerate {
+          inherit pkgs;
+          inherit format;
+          modules = [
+            config.flake.nixosModules.server
+          ];
+        }
+      );
+in
 {
   imports = [
-    ./aarch64
-
     ../roles
   ];
+
+  flake.installers = {
+    x86_64 = image {
+      system = "x86_64-linux";
+      format = "install-iso";
+    };
+
+    aarch64 = image {
+      system = "aarch64-linux";
+      format = "sd-aarch64-installer";
+    };
+  };
 }
