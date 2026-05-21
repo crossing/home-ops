d07e39308d3b7b6ee35b83c44e030d41567a9bed
Xing Yang <xor@jecity.net>
2026-05-21 12:07:30 +0100

    overlay/unstable: inherit vscode from unstable channel

--- overlays/unstable/default.nix
+++ overlays/unstable/default.nix
@@ -2,6 +2,6 @@
 , ...
 }:
 self: super: {
-  inherit (channels.nixpkgs-unstable) docker gws unifi _1password-gui _1password-cli;
+  inherit (channels.nixpkgs-unstable) docker gws unifi _1password-gui _1password-cli vscode;
   inherit (channels.nixpkgs-old) mongodb-7_0;
 }
